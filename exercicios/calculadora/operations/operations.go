9b8fef1d4c1e294fea8d4bc2fd9601b1bb10e226
Carlos <55070485+git-mare@users.noreply.github.com>
2023-08-28 22:36:37 -0300

    Create operations.go

---
+++ exercicios/calculadora/operations/operations.go
@@ -0,0 +1,17 @@
+package operations
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Div(a, b int) int {
+	return a / b
+}
+
+func Mult(a, b int) int {
+	return a * b
+}
