9139e679fe921de9e6758e89b22108608e9a2b76
Carlos <55070485+git-mare@users.noreply.github.com>
2023-08-29 01:54:01 -0300

    Update main.go

--- exercicios/calculadora/main.go
+++ exercicios/calculadora/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"calculadora/operations"
-	"fmt"
+	"calculadora/console"
 )
 
 func main() {
-	op := operations.Add(1, 2)
-	comp := operations.Comp(1, 2)
-	fmt.Printf("%v\n%v", op, comp)
+	console.Calc()
 }
